616546d748f2a457d13ba6baedce05441821b052
Alexandre Athanasakis <42991@etu.he2b.be>
2021-08-03 18:17:49 +0200

    Adding 'templates' and first view

--- mproject/developer/views.py
+++ mproject/developer/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+from .models import Developer
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the developers index.")
+    context = {
+        'developers': Developer.objects.all()
+    }
+    return render(request, 'developer/index.html', context)
