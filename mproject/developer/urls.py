715e94a5e1a0d1c488ea46ea70c7ebe54663264e
Alexandre Athanasakis <42991@etu.he2b.be>
2021-08-04 20:19:19 +0200

    Converting actual system into generic view (Mixin)

--- mproject/developer/urls.py
+++ mproject/developer/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+#import locaux
 from . import views
+from .views import DevDetailVue, IndexView
 
 app_name = 'developer'
 
@@ -7,7 +9,7 @@ app_name = 'developer'
 #   '' = /developer (car on est ici dans l'application developer)
 #   '<int:developer_id>' = /developer/<int:developer_id>
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<int:developer_id>', views.detail, name='detail'), 
+    path('', IndexView.as_view(), name='index'), 
+    path('<int:pk>', DevDetailVue.as_view(), name='detail'),
     path('create', views.create, name='create'),
 ]
--- mproject/developer/views.py
+++ mproject/developer/views.py
@@ -2,23 +2,25 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views.generic import DetailView, ListView
 #imports locaux
 from .models import Developer
 from .forms import DeveloperForm
 
 # Liste des developpeurs
-def index(request):
-    context = {
-        'developers': Developer.objects.all(),
-        'form': DeveloperForm,
-    }
-    return render(request, 'developer/index.html', context)
+class IndexView(ListView): 
+    model = Developer 
+    template_name = "developer/index.html" 
+    context_object_name = 'developers'
+    def get_context_data(self, **kwargs): 
+        context = super(IndexView, self).get_context_data(**kwargs) 
+        context['form'] = DeveloperForm 
+        return context
 
-# Details d'un developpeur
-def detail(request, developer_id): 
-    #developer = Developer.objects.get(pk=developer_id) 
-    developer = get_object_or_404(Developer, pk=developer_id)
-    return render(request, 'developer/detail.html', {'developer': developer})
+# Details concernant un developpeur
+class DevDetailVue(DetailView):
+    model = Developer
+    template_name = 'developer/detail.html'
 
 def create(request): 
     form = DeveloperForm(request.POST) 
