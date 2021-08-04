108773a9e8cf22d60c74a7aceb04ef62eaa9c8b8
Alexandre Athanasakis <42991@etu.he2b.be>
2021-08-04 23:30:54 +0200

    Including tests with TestCase

--- mproject/developer/models.py
+++ mproject/developer/models.py
@@ -5,6 +5,11 @@ from django.db import models
 class Developer(models.Model):
     first_name = models.CharField("first name", max_length=200)
     last_name = models.CharField(max_length=200)
+
+    # Vérifie si un dev n'a aucune taches.
+    def is_free(self):
+        return self.tasks.count() == 0
+
     # Definir la valeur de retour dans le shell.
     # __str__ en python est l'équilalant de toString() en java.
     def __str__(self):
--- mproject/developer/tests.py
+++ mproject/developer/tests.py
@@ -1,3 +1,75 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Developer
 
-# Create your tests here.
+# La classe hérite de TestCase afin d'ètre reconnue.
+# Pour lancer les tests :
+# => python manage.py test developer
+class DeveloperModelTests(TestCase):
+    def setUp(self):
+        Developer.objects.create(first_name="Sébastien", last_name="Drobisz")
+
+    # Une fonction dois commencer par test pour etre reconnue.
+    def test_is_free_with_no_tasks(self):
+        """
+        is_free() returns True for developer with no
+        tasks.
+        """
+        dev = Developer.objects.get(first_name="Sébastien")
+        self.assertIs(dev.is_free(), True)
+
+    def test_is_free_with_one_tasks(self):
+        """
+        is_free() returns False for developer with at least one
+        tasks.
+        """
+        dev = Developer.objects.get(first_name="Sébastien")
+        dev.tasks.create(title="cours Django", description="Faire le cours sur Django")
+        self.assertIs(dev.is_free(), False)
+
+class DeveloperIndexViewTests(TestCase):
+    def test_no_developers(self):
+        """
+        If no developers exist, an appropriate message is displayed.
+        """
+        response = self.client.get(reverse('developer:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertContains(response, "Il n'y a aucune dévelopeur enregistré !")
+        self.assertQuerysetEqual(response.context['developers'], [])
+
+    def test_one_developer(self):
+        """
+        A developer is displayed on the index page.
+        """
+        dev = Developer.objects.create(
+        first_name="Jonathan",
+        last_name="Lechien")
+        response = self.client.get(reverse('developer:index'))
+        self.assertEquals(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['developers'],
+        [f'<Developer: {dev.first_name} {dev.last_name}>'])
+        self.assertContains(response, dev.first_name)
+
+class DevDetailView(TestCase):
+    def test_existing_developer(self):
+        """
+        The detail view of a developer displays the developer's text.
+        """
+        dev = Developer.objects.create(
+        first_name="Jonathan",
+        last_name="Lechien")
+        url = reverse('developer:detail', args=(dev.id,))
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.context['developer'], dev)
+        self.assertContains(response, dev.first_name)
+        self.assertContains(response, dev.last_name)
+
+    def test_non_existing_developer(self):
+        """
+        The detail view of a non existing developer should return 404
+        status_code response.
+        """
+        url = reverse('developer:detail', args=(1,))
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 404)
