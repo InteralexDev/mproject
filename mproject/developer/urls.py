549138ef9d44c8e57f0b88030d9a77c14ce47e36
Alexandre Athanasakis <42991@etu.he2b.be>
2021-08-04 19:17:39 +0200

    Link between views and developer specifics added

--- mproject/developer/urls.py
+++ mproject/developer/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'developer'
+
+# Les urls de l'application :
+#   '' = /developer (car on est ici dans l'application developer)
+#   'specifics/<int:developer_id>' = /developer/specifics/<int:developer_id>
 urlpatterns = [
-path('', views.index, name='index'),
+    path('', views.index, name='index'),
+    path('specifics/<int:developer_id>', views.detail, name='detail'), 
 ]
--- mproject/developer/views.py
+++ mproject/developer/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import HttpResponse
 
 from .models import Developer
 
+# Liste des developpeurs
 def index(request):
     context = {
         'developers': Developer.objects.all()
     }
     return render(request, 'developer/index.html', context)
+
+# Details d'un developpeur
+def detail(request, developer_id): 
+    #developer = Developer.objects.get(pk=developer_id) 
+    developer = get_object_or_404(Developer, pk=developer_id)
+    return render(request, 'developer/detail.html', {'developer': developer})
